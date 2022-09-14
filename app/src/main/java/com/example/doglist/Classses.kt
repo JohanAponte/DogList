efafc34ce55cb07f3848571ba3e7be98cddb0a1d
Johan Aponte <johan.daniel.aponte94@gmail.com>
2022-09-14 08:47:05 -0500

    [ADD] Initial files

---
+++ app/src/main/java/com/example/doglist/Classses.kt
@@ -0,0 +1,11 @@
+package com.example.doglist
+
+data class Classses(
+    val accessibility: Double,
+    val activity: String,
+    val key: String,
+    val link: String,
+    val participants: Int,
+    val price: Int,
+    val type: String
+)
